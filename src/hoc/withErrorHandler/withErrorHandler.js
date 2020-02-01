3abc8ddb0bc77f7c0677db5fe55f7b9138014bf6
Hugo Sant'Ana <hugostn@gmail.com>
2020-02-01 16:56:21 -0300

    test

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -19,6 +19,8 @@ const withErrorHandler = (WrappedComponents, axios) => {
             axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error});
             })
+
+            //teste
         }
 
         errorConfirmedHandler = () => {
